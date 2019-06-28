aa2fc137c8ff6f09cc884cdc7a4411db4e1fcf90
Ming <18605540702@163.com>
2019-06-28 14:14:18 +0800

    Vue框架修改

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="navbar">
+        <div class="container t-le">
+            <img src="../assets/logo-white.png" width="105" height="46">
+        </div>
+    </div>
+</template>
+
+<style>
+.navbar{
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 0;
+    width: 100%;
+    line-height: 1;
+    background: #50AC02;
+}
+.navbar .container{
+    padding: 10px 12px;
+    background: url("../assets/navbar.png") no-repeat;
+    background-position: top right;
+}
+</style>
---
+++ src/components/NodeVersion.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul id="NodeVersion" class="mt1">
+    <li v-if="items">主网版本<br/><span>Mainnet Version : {{items.mainnet}}</span></li>
+    <li v-if="items" class="green">测试网版本<br/><span>Testnet Version : {{items.testnet}}</span></li>
+  </ul>
+</template>
+
+<script>
+// /api/consensus
+export default {
+  name: 'NodeVersion',
+  data () {
+    return {items :''}
+  },
+  mounted () {
+    this.getVersion();
+  },
+  methods: {
+    getVersion:function() {
+      this.$http.get('/api/consensus').then(function(res){
+        this.items = res.data;
+      },function(){
+        console.log("未调用到api");
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#NodeVersion{
+  padding: 0;
+}
+#NodeVersion li{
+  display: inline-block;
+  margin: 25px;
+  width: 220px;
+  height: 61px;
+  list-style: none;
+  font-size: 20px;
+  color:#50AC02;
+  background: #FFFFFF;
+  line-height: 1.3;
+  letter-spacing: .3px;
+  padding-top: 9px;
+}
+#NodeVersion li.green{
+  color:#FFFFFF;
+  background: #50AC02;
+  box-shadow: 1px 1px 4px #D7D6D6;
+}
+#NodeVersion li span{
+  font-size: 14px;
+}
+</style>
